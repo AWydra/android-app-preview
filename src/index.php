8c20996adde9853bb67fe493b1a652e516f9ad13
AWydra <hello@awydra.pl>
2019-03-27 15:58:47 +0100

    Added files from fastcast4u

--- src/index.php
+++ src/index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Android App Preview</title>
+    <?php
+        include('head.php');
+    ?>
+    <title>FastCast4u | ANDROID APP PREVIEW</title>
 </head>
+
 <body>
-    
+    <?php
+        include('menu.php');
+    ?>
+
+    <!-- Document Wrapper
+============================================= -->
+    <div id="wrapper" class="clearfix w-100">
+        <header class="heading-block center col-12 pt-5" style="margin: 0 auto;">
+            <h2>ANDROID APP PREVIEW</h2>
+            <span class="divcenter mt-3">Easily create sub-accounts for individual stations in
+                Centova Cast admin panel. You set the limits!</span>
+        </header>
+        <main>
+        <iframe src="app.php" frameborder="0" style="width:320px;height:560px;"></iframe>
+        </main>
+
+
+        <!-- Footer
+    ============================================= -->
+        <footer id="footer" class="dark">
+
+            <?php
+        include('footer.php');
+        ?>
+
+        </footer>
+        <!-- #footer end -->
+
+    </div>
+    <!-- #wrapper end -->
 </body>
+
 </html>
