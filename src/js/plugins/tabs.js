f3fb6a6a61e27197c799b259fd5aebc54f23a550
AWydra <hello@awydra.pl>
2019-03-28 14:47:01 +0100

    Just tabs

---
+++ src/js/plugins/tabs.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabLinks = document.querySelectorAll('.tabs');
+
+  tabLinks.forEach((el) => {
+    el.onclick = function () {
+      const target = this.getAttribute('href').replace('#', '');
+      const sections = document.querySelectorAll('.tab-content');
+
+      sections.forEach(el => {
+        el.style.display = 'none';
+      })
+
+      document.getElementById(target).style.display = 'flex';
+
+      tabLinks.forEach(el => {
+        el.classList.remove('is-active');
+      })
+
+      this.classList.add('is-active');
+
+      return false;
+    }
+  })
+}, false);
