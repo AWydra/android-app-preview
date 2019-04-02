7d79ef28e8877ac83de74104878ab98271c8e0b9
AWydra <hello@awydra.pl>
2019-04-02 15:58:04 +0200

    Added animations

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,27 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const why = new SimpleBar(document.querySelector('aside'));
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    const why = new SimpleBar(document.querySelector("aside"));
 
-  //Menu
-  const otterNav = document.querySelector('#otter-nav');
-  const otterBtn = document.querySelector('#otter-btn');
-  const overlay = document.querySelector('.main-overlay');
-  const navItem = document.querySelectorAll('.navigation__item');
+    //Menu
+    const otterNav = document.querySelector("#otter-nav");
+    const otterBtn = document.querySelector("#otter-btn");
+    const radioBtn = document.querySelectorAll(".player-button");
+    const overlay = document.querySelector(".main-overlay");
+    const navItem = document.querySelectorAll(".navigation__item");
+    const artistContainer = document.querySelector(".radio__artist");
 
-  otterBtn.addEventListener('click', () => {
-    otterNav.classList.add('navigation__active');
-    overlay.classList.remove('hidden');
-  });
-
-  overlay.addEventListener('click', () => {
-    otterNav.classList.remove('navigation__active');
-    overlay.classList.add('hidden');
-  });
-
-  navItem.forEach((el) => {
-    el.addEventListener('click', () => {
-      otterNav.classList.remove('navigation__active');
-      overlay.classList.add('hidden');
+    // Open navigation
+    otterBtn.addEventListener("click", () => {
+      otterNav.classList.add("navigation__active");
+      overlay.classList.remove("hidden");
     });
-  });
 
-}, false);
+    // Click play button
+    radioBtn.forEach(el => {
+      el.addEventListener("click", function() {
+        artistContainer.classList.toggle("fadeInUp");
+        artistContainer.classList.toggle("fadeOutDown");
+        artistContainer.classList.remove("hidden");
+      });
+    });
+
+    // Clicko navigation overlay
+    overlay.addEventListener("click", () => {
+      otterNav.classList.remove("navigation__active");
+      overlay.classList.add("hidden");
+    });
+
+    // Close navigation after click item
+    navItem.forEach(el => {
+      el.addEventListener("click", () => {
+        otterNav.classList.remove("navigation__active");
+        overlay.classList.add("hidden");
+      });
+    });
+
+    //Remove chat wigdet
+    setTimeout(() => {
+      document
+        .querySelector("#web iframe")
+        .contentDocument.querySelectorAll('iframe[src="about:blank"]')
+        .forEach(el => {
+          el.remove();
+        });
+    }, 5000);
+  },
+  false
+);
