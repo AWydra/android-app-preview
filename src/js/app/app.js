fa980fbbb2cbb792f6d4c0cfafe45d403f14dc27
AWydra <hello@awydra.pl>
2019-04-10 18:36:50 +0200

    Code divided into functions

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,54 +1,45 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    const why = new SimpleBar(document.querySelector("aside"));
+function appInit() {
+  const why = new SimpleBar(document.querySelector("aside"));
 
-    //Menu
-    const otterNav = document.querySelector("#otter-nav");
-    const otterBtn = document.querySelector("#otter-btn");
-    const radioBtn = document.querySelectorAll(".player-button");
-    const overlay = document.querySelector(".main-overlay");
-    const navItem = document.querySelectorAll(".navigation__item");
-    const artistContainer = document.querySelector(".radio__artist");
+  //Menu
+  const otterNav = document.querySelector("#otter-nav");
+  const otterBtn = document.querySelector("#otter-btn");
+  const radioBtn = document.querySelectorAll(".player-button");
+  const overlay = document.querySelector(".main-overlay");
+  const artistContainer = document.querySelector(".radio__artist");
 
-    // Open navigation
-    otterBtn.addEventListener("click", () => {
-      otterNav.classList.add("navigation__active");
-      overlay.classList.remove("hidden");
+  // Open navigation
+  otterBtn.addEventListener("click", () => {
+    otterNav.classList.add("navigation__active");
+    overlay.classList.remove("hidden");
+  });
+
+  // Click play button
+  radioBtn.forEach(el => {
+    el.addEventListener("click", function() {
+      artistContainer.classList.toggle("fadeInUp");
+      artistContainer.classList.toggle("fadeOutDown");
+      artistContainer.classList.remove("hidden");
     });
+  });
 
-    // Click play button
-    radioBtn.forEach(el => {
-      el.addEventListener("click", function() {
-        artistContainer.classList.toggle("fadeInUp");
-        artistContainer.classList.toggle("fadeOutDown");
-        artistContainer.classList.remove("hidden");
-      });
-    });
+  // Clicko navigation overlay
+  overlay.addEventListener("click", () => {
+    otterNav.classList.remove("navigation__active");
+    overlay.classList.add("hidden");
+  });
+}
 
-    // Clicko navigation overlay
-    overlay.addEventListener("click", () => {
+function appNav() {
+  const otterNav = document.querySelector("#otter-nav");
+  const overlay = document.querySelector(".main-overlay");
+  const navItem = document.querySelectorAll(".navigation__item");
+
+  // Close navigation after click item
+  navItem.forEach(el => {
+    el.addEventListener("click", () => {
       otterNav.classList.remove("navigation__active");
       overlay.classList.add("hidden");
     });
-
-    // Close navigation after click item
-    navItem.forEach(el => {
-      el.addEventListener("click", () => {
-        otterNav.classList.remove("navigation__active");
-        overlay.classList.add("hidden");
-      });
-    });
-
-    //Remove chat wigdet
-    setTimeout(() => {
-      document
-        .querySelector("#web iframe")
-        .contentDocument.querySelectorAll('iframe[src="about:blank"]')
-        .forEach(el => {
-          el.remove();
-        });
-    }, 5000);
-  },
-  false
-);
+  });
+}
--- src/js/plugins/tabs.js
+++ src/js/plugins/tabs.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const tabLinks = document.querySelectorAll('.tabs');
+function tabs() {
+  const tabLinks = document.querySelectorAll(".tabs");
 
-  tabLinks.forEach((el) => {
-    el.onclick = function () {
-      const target = this.getAttribute('href').replace('#', '');
-      const sections = document.querySelectorAll('.tab-content');
+  tabLinks.forEach(el => {
+    el.onclick = function() {
+      const target = this.getAttribute("href").replace("#", "");
+      const sections = document.querySelectorAll(".tab-content");
 
       sections.forEach(el => {
-        el.style.display = 'none';
-      })
+        el.style.display = "none";
+      });
 
-      document.getElementById(target).style.display = 'flex';
+      document.getElementById(target).style.display = "flex";
 
       tabLinks.forEach(el => {
-        el.classList.remove('is-active');
-      })
+        el.classList.remove("is-active");
+      });
 
-      this.classList.add('is-active');
+      this.classList.add("is-active");
 
       return false;
-    }
-  })
-}, false);
+    };
+  });
+}
