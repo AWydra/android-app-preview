9c31e4774260a0d71660fb91b76b9569c391d223
Arkadiusz Wydra <hello@awydra.pl>
2019-05-01 22:19:45 +0200

    Added function for loading values from JSON

--- src/js/creator/config.js
+++ src/js/creator/config.js
@@ -1,3 +1,34 @@
+const initConfig = () => {
+  appName();
+  slideBg();
+  slideColor();
+  navbarBg();
+  navbarColor();
+  slideHeading();
+  notificationBg();
+  playButton();
+  playButtonIcon();
+};
+
+const loadConfig = config => {
+  //General
+  for (const key in config.general) {
+    document.querySelector(`[name="${key}"]`)
+      ? (document.querySelector(`[name="${key}"]`).value = config.general[key])
+      : "";
+  }
+
+  //Color
+  for (const key in config.color) {
+    document
+      .querySelector(`[name="${key}"]`)
+      .jscolor.fromString(config.color[key]);
+    slideBg();
+  }
+
+  initConfig();
+};
+
 const sendConfig = () => {
   const form = document.querySelector("#preview-form");
   const data = new FormData(form);
@@ -10,7 +41,7 @@ const sendConfig = () => {
     processData: false,
     contentType: false,
     success: function() {
-      console.log("Send");
+      console.log("Sent");
     }
   });
 };
