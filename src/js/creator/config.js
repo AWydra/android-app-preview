7a67dc07374dd22234e069bfd3f270a5454370cf
Arkadiusz Wydra <hello@awydra.pl>
2019-05-03 00:02:25 +0200

    Extended function for build navigation from JSON

--- src/js/creator/config.js
+++ src/js/creator/config.js
@@ -26,6 +26,155 @@ const loadConfig = config => {
     slideBg();
   }
 
+  //Navigation
+  const navContainer = document.querySelector(".otter-sort-container");
+  config.navigation.forEach(el => {
+    const addBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+    const li = document.createElement("li");
+    li.classList.add("p1", "mb1", "navy", "bg-yellow", "position-relative");
+    li.innerHTML = `
+  <h4><input class="section-heading" type="text" value="${
+    el[0]
+  }" required /></h4>
+  <ul class="otter-sort-item js-sortable sortable list list-reset" aria-dropeffect="move">
+    <li class="navigation__btn">
+    </li>
+  </ul>`;
+
+    addBtn.classList.add(
+      "item-add",
+      "button",
+      "button-rounded",
+      "button-small"
+    );
+    addBtn.textContent = `Add More`;
+    addBtn.addEventListener("click", addItem);
+    li.querySelector(".navigation__btn").appendChild(addBtn);
+
+    removeBtn.classList.add("remove");
+    removeBtn.innerHTML = `<i class="icon-line-circle-cross"></i>`;
+    removeBtn.addEventListener("click", removeSection);
+
+    li.insertBefore(removeBtn, li.firstChild);
+    navContainer.appendChild(li);
+
+    sortable(".otter-sort-container", {
+      forcePlaceholderSize: true,
+      placeholderClass: "ph-class",
+      hoverClass: "bg-maroon yellow"
+    });
+
+    sortable(".otter-sort-item", {
+      forcePlaceholderSize: true,
+      placeholderClass: "ph-class",
+      hoverClass: "bg-maroon yellow",
+      acceptFrom: ".otter-sort-item"
+    });
+
+    const navItemContainer = li.querySelector(".otter-sort-item");
+
+    el.forEach((el, i) => {
+      if (i === 0) return;
+      const removeBtn = document.createElement("button");
+      const li = document.createElement("li");
+      li.classList.add(
+        "p1",
+        "mb1",
+        "navy",
+        "bg-yellow",
+        "row",
+        "position-relative"
+      );
+      //li.setAttribute("data-tabs", "#web");
+      li.innerHTML = `<div class="form-group col-2 m-0">
+                  <select class="iconpicker-select">
+                    <option data-value="hidden" value="hidden">No icon</option>
+                    <option data-value="ottericon_signal">otterIcon-signal</option>
+                    <option data-value="ottericon_tv">otterIcon-tv</option>
+                    <option data-value="ottericon_fb">otterIcon-fb</option>
+                    <option data-value="icon_line2_social_twitter">icon-line2-social-twitter</option>
+                    <option data-value="ottericon_instagram">otterIcon-instagram</option>
+                    <option data-value="icon_line2_globe">icon-line2-globe</option>
+                    <option data-value="ottericon_wordpress">otterIcon-wordpress</option>
+                    <option data-value="icon_line2_settings">icon-line2-settings</option>
+                    <option data-value="icon_line_bell">icon-line-bell</option>
+                    <option data-value="icon_bell">icon-bell</option>
+                    <option data-value="icon_line2_bell">icon-line2-bell</option>
+                    <option data-value="icon_line_video">icon-line-video</option>
+                    <option data-value="icon_line_heart">icon-line-heart</option>
+                    <option data-value="icon_heart2">icon-heart2</option>
+                    <option data-value="icon_line_microphone">icon-line-microphone</option>
+                    <option data-value="icon_microphone">icon-microphone</option>
+                    <option data-value="icon_microphone2">icon-microphone2</option>
+                    <option data-value="icon_line_download">icon-line-download</option>
+                    <option data-value="icon_line_globe">icon-line-globe</option>
+                    <option data-value="icon_line_bar_graph">icon-line-bar-graph</option>
+                    <option data-value="icon_comment">icon-comment</option>
+                    <option data-value="icon_location">icon-location</option>
+                    <option data-value="icon_call">icon-call</option>
+                    <option data-value="icon_line_star">icon-line-star</option>
+                    <option data-value="icon_line2_star">icon-line2-star</option>
+                    <option data-value="icon_star3">icon-star3</option>
+                    <option data-value="icon_home2">icon-home2</option>
+                    <option data-value="icon_play">icon-play</option>
+                    <option data-value="icon_play_sign">icon-play-sign</option>
+                    <option data-value="icon_line2_control_play">icon-line2-control-play</option>
+                    <option data-value="icon_googleplay">icon-googleplay</option>
+                    <option data-value="icon_line_play">icon-line-play</option>
+                    <option data-value="icon_share">icon-share</option>
+                    <option data-value="icon_line_share">icon-line-share</option>
+                    <option data-value="icon_share2">icon-share2</option>
+                  </select>
+                </div>
+                <div class="form-group col-12 col-sm-5 mt-3 m-sm-0">
+                  <label class="text-white d-block m-0">Name: </label>
+                  <input class="item-name w-100" type="text" value="${
+                    el[1]
+                  }" required />
+                </div>
+                <div class="form-group col-12 col-sm-5 mt-3 m-sm-0">
+                  <label class="text-white d-block m-0">URL </label>
+                  <input
+                    class="item-url w-100"
+                    type="url"
+                    placeholder="http://www.example.com"
+                    value="${el[2]}"
+                    required
+                  />
+                </div>`;
+      li.querySelector(`option[data-value="${el[0]}"]`).setAttribute(
+        "selected",
+        ""
+      );
+
+      removeBtn.classList.add("remove", "remove--item");
+      removeBtn.innerHTML = `<i class="icon-line-circle-cross"></i>`;
+      removeBtn.addEventListener("click", removeSection);
+      li.appendChild(removeBtn);
+      navItemContainer.appendChild(li);
+
+      sortable(".otter-sort-item", {
+        forcePlaceholderSize: true,
+        placeholderClass: "ph-class",
+        hoverClass: "bg-maroon yellow",
+        acceptFrom: ".otter-sort-item"
+      });
+
+      $(".iconpicker-select")
+        .fontIconPicker({
+          emptyIcon: false
+        })
+        .on("change", () => {
+          navOtter();
+        });
+      zIndex();
+      navOtter();
+    });
+    zIndex();
+    navOtter();
+  });
+
   initConfig();
 };
 
