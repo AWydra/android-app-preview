0ce6a89d69ad5ad46e327924dd6f5606fd30cfa6
AWydra <hello@awydra.pl>
2019-05-09 18:27:26 +0200

    Rewrited for valid JSON and added default values

--- src/js/creator/structure.js
+++ src/js/creator/structure.js
@@ -1,40 +1,37 @@
 const createStructure = () => {
-  const structure = {};
-
-  //General
-  structure.general = {};
-  structure.general.appName = "appNameValue";
-  structure.general.admob = "admobValue";
-  structure.general.push = "pushValue";
-  structure.general.message = "messageValue";
-
-  //Navigation
-  structure.navigation = navStructure;
-
-  //Color
-  structure.color = {};
-  structure.color.slideBg = "000000";
-  structure.color.slideColor = "FFFFFF";
-  structure.color.slideHeading = "DDDDDD";
-  structure.color.notificationBg = "9C9C9C";
-  structure.color.navbarBg = "000000";
-  structure.color.navbarColor = "FFFFFF";
-  structure.color.playButton = "000000";
-  structure.color.playButtonIcon = "FFFFFF";
-  structure.color.launchScreenColor = "FFFFFF";
-
-  //Checkbox
-  structure.checkbox = {};
-  structure.checkbox.menuLogo = "Logo";
-  structure.checkbox.coverDisplay = "Fixed Logo";
-  structure.checkbox.appLogo = "Logo";
-  structure.checkbox.launchScreenLogo = "Upload custom";
-
-  //Image URLs
-  structure.image = {};
-  structure.image.menuLogoFile = "https://picsum.photos/200/300";
-  structure.image.appLogoFile = "https://picsum.photos/300/200";
-  structure.image.launchScreenLogoFile = "https://picsum.photos/200/300";
+  const structure = {
+    general: {
+      appName: "appNameValue",
+      admob: "admobValue",
+      push: "pushValue",
+      message: "messageValue"
+    },
+    navigation: JSON.parse(
+      '[["Home",["ottericon_signal","FastCast4u Radio",""]],["Social Media",["ottericon_fb","Facebook","https://www.facebook.com/"],["icon_line2_social_twitter","Twitter","https://www.twitter.com/"],["ottericon_instagram","Instagram","https://www.instagram.com/"],["icon_line_heart","Best site","https://www.w3.org/"],["icon_line_globe","Example.com","https://www.example.com/"]],["Content",["icon_line2_globe","Your website","https://www.myblog.com/"],["icon_line2_globe","Custom website","https://www.custom.com/"],["ottericon_wordpress","Your wordpress blog","https://www.wordpress.com/"]],["Misc",["icon_line2_settings","Settings",""]]]'
+    ),
+    color: {
+      slideBg: "000000",
+      slideColor: "FFFFFF",
+      slideHeading: "DDDDDD",
+      notificationBg: "9C9C9C",
+      navbarBg: "000000",
+      navbarColor: "FFFFFF",
+      playButton: "000000",
+      playButtonIcon: "FFFFFF",
+      launchScreenColor: "FFFFFF"
+    },
+    checkbox: {
+      menuLogo: "Logo",
+      coverDisplay: "Fixed Logo",
+      appLogo: "Logo",
+      launchScreenLogo: "Upload custom"
+    },
+    image: {
+      menuLogoFile: "https://picsum.photos/200/300",
+      appLogoFile: "https://picsum.photos/300/200",
+      launchScreenLogoFile: ""
+    }
+  };
 
   return structure;
 };
